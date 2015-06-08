087fb4a7bec0e21c66f28c1a5a36badd50e7e75c
Ming Li <li.ming116@gmail.com>
2015-06-08 22:00:27 +0100

    Use a thread to send email notification.

--- app/emails.py
+++ app/emails.py
@@ -1,11 +1,19 @@
 from flask.ext.mail import Message
-from app import mail
+from threading import Thread
+from app import mail, app
+from .decorators import async
+
+@async
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
 
 from flask import render_template
 from config import ADMINS
